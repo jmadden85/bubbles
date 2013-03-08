b456237eff08e82333061d2d49f6b12cd2a798d1
Joe Madden <joe@beardbro.com>
2013-03-08 01:29:45 -0500

    bubble hovered

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 var bubbleHandler = function () {
 
+    $('.bubble').hover(function() {
+        var that = $(this);
+        var thatRadius = that.width() / 2;
+        that.on('mousemove', function(e) {
+            var x = e.offsetX;
+            var y = e.offsetY;
+            var xCalc = (x - thatRadius) * (x - thatRadius);
+            var yCalc = (y - thatRadius) * (y - thatRadius);
+            var inCircle = Math.sqrt(xCalc + yCalc) <= thatRadius;
+        });
+    }, function() {
+        moused = false
+    });
+
     function bubbleHovered (el) {
-
-
 
     };
 
